fb05332aa54fb4cd473331bf6a68a1dc77afdd46
Romain F. T <rrroschan@gmail.com>
2020-05-01 01:22:15 +0200

    Build prefs pages in individual methods

--- README.md
+++ README.md
@@ -8,9 +8,8 @@ A GNOME Shell extension providing customizable sticky notes.
 
 ## Installation
 
-From [here](https://extensions.gnome.org/extension/1357/notes/)
-
-Or download & extract the ZIP, and run `./install.sh`
+- From [here](https://extensions.gnome.org/extension/1357/notes/)
+- Or download & extract the ZIP, and run `./install.sh`
 
 ## Storage
 
@@ -22,9 +21,12 @@ Notes are stored in files at `~/.local/share/notes@maestroschan.fr/*_text`
 <!-- 3.36 :
 
 le putain de focus
+le packing des boutons de couleur
 rétrocompatibilité en sang
-déprécation de l'acteur de l'icône avec conditionnelle
+déprécation de l'acteur de l'icône, avec conditionnelle
 autres erreurs de merde ?
+
+sa mère faut réécrire comment ça interagit avec le disque, là c'est hoooonteux
 
     -->
 
--- notes@maestroschan.fr/prefs.js
+++ notes@maestroschan.fr/prefs.js
@@ -12,8 +12,6 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-//------------------------------------------------------------------------------
-
 function init() {
 	Convenience.initTranslations();
 }
@@ -26,26 +24,34 @@ const NotesSettingsWidget = new GObject.Class({
 	Name: 'NotesSettingsWidget',
 	GTypeName: 'NotesSettingsWidget',
 
-	_init: function() {
+	_init () {
 		let builder = new Gtk.Builder();
 		builder.add_from_file(Me.path+'/prefs.ui');
 		this.prefs_stack = builder.get_object('prefs_stack');
-		
+
 		this.switcher = new Gtk.StackSwitcher({
 			halign: Gtk.Align.CENTER,
 			visible: true,
 			stack: this.prefs_stack
 		});
-		
-		//----------------------------------------------------------------------
-		
+
+		this._buildSettingsPage(builder);
+		this._buildHelpPage(builder);
+		this._buildAboutPage(builder);
+
+		this.switcher.show_all();
+	},
+
+	//--------------------------------------------------------------------------
+
+	_buildSettingsPage (builder) {
+
 		let position_combobox = builder.get_object('position_combobox');
-		
+
 		position_combobox.append('on-background', _("On the background"));
 		position_combobox.append('above-all', _("Above everything"));
-	
+
 		position_combobox.active_id = SETTINGS.get_string('layout-position');
-		
 		position_combobox.connect('changed', (widget) => {
 			SETTINGS.set_string('layout-position', widget.get_active_id());
 			if( (widget.get_active_id() == 'above-all') ) {
@@ -103,8 +109,11 @@ const NotesSettingsWidget = new GObject.Class({
 				hide_switch.sensitive = false;
 			}
 		});
+	},
 
-//		//----------------------------------------------------------------------
+	//--------------------------------------------------------------------------
+
+	_buildHelpPage(builder) {
 
 		builder.get_object('image1').set_from_pixbuf(
 			GdkPixbuf.Pixbuf.new_from_file_at_size(
@@ -125,42 +134,43 @@ const NotesSettingsWidget = new GObject.Class({
 			SETTINGS.set_boolean('ugly-hack', !SETTINGS.get_boolean('ugly-hack'));
 		});
 
-		//----------------------------------------------------------------------
+		// TODO revoir le ficher UI
+		// là la stack est dans une scrolledwindow c'est nul
+	},
+
+	//--------------------------------------------------------------------------
+
+	_buildAboutPage(builder) {
 		
 		builder.get_object('about_icon').set_from_pixbuf(
 			GdkPixbuf.Pixbuf.new_from_file_at_size(Me.path +
 			                         '/screenshots/about_picture.png', 163, 114)
-//			                         '/screenshots/about_picture.png', 326, 228)
+			//                         '/screenshots/about_picture.png', 326, 228)
 		);
-		
+
 		let a_version = _("version:") + ' ' + Me.metadata.version.toString();
 		builder.get_object('label_version').set_label(a_version)
-		
+
 		let translation_credits = builder.get_object('translation_credits').get_label();
 		if (translation_credits == 'translator-credits') {
 			builder.get_object('translation_label').set_label('');
 			builder.get_object('translation_credits').set_label('');
 		}
-		
+
 		let linkBox = builder.get_object('link_box');
-		
 		let url_button1 = new Gtk.LinkButton({
 			label: _("Report bugs or ideas"),
 			uri: Me.metadata.url.toString()
 		});
-		
 		linkBox.pack_start(url_button1, false, false, 0);
-		
-		//----------------------------------------------------------------------
-		
-		this.switcher.show_all();
 	}
+
 });
 
-//-----------------------------------------------
+//------------------------------------------------------------------------------
 
-//I guess this is like the "enable" in extension.js : something called each
-//time he user try to access the settings' window
+// I guess this is like the "enable" in extension.js : something called each
+// time he user try to access the settings' window
 function buildPrefsWidget() {
 	let widget = new NotesSettingsWidget();
 	Mainloop.timeout_add(0, () => {
@@ -172,4 +182,5 @@ function buildPrefsWidget() {
 	return widget.prefs_stack;
 }
 
-//-----------------------------------------------
+//------------------------------------------------------------------------------
+
