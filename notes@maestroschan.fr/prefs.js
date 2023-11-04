d7b9e8c1b51c7140247bf8fec5a1651f01ca7791
Gianni Lerro <glerro@pm.me>
2023-11-04 21:15:55 +0100

    Turn preferences into module, use new convenience APIs & port to ESM all Gi/GS imports.
    * This is an initial working port, but no use adwaita widgets amd styles.

--- notes@maestroschan.fr/prefs.js
+++ notes@maestroschan.fr/prefs.js
@@ -2,20 +2,15 @@
 // GPL v3
 // Copyright 2018-2021 Romain F. T.
 
-const{ GObject, Gtk, Gdk, GdkPixbuf, GLib } = imports.gi;
-const Mainloop = imports.mainloop;
+import GObject from 'gi://GObject';
+import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk';
+import Gdk from 'gi://Gdk';
+import GdkPixbuf from 'gi://GdkPixbuf';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 
-const Gettext = imports.gettext.domain('notes-extension');
-const _ = Gettext.gettext;
-
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-
-function init() {
-	ExtensionUtils.initTranslations();
-}
-
-let SETTINGS = ExtensionUtils.getSettings();
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 //------------------------------------------------------------------------------
 
@@ -23,9 +18,13 @@ const NotesSettingsWidget = new GObject.Class({
 	Name: 'NotesSettingsWidget',
 	GTypeName: 'NotesSettingsWidget',
 
-	_init () {
+	_init (extension) {
+		this._extension = extension;
+		this._settings = this._extension.getSettings();
+		this._path = this._extension.path;
+
 		let builder = new Gtk.Builder();
-		builder.add_from_file(Me.path+'/prefs.ui');
+		builder.add_from_file(this._path+'/prefs.ui');
 		this.prefs_stack = builder.get_object('prefs_stack');
 
 		this._buildSettingsPage(builder);
@@ -42,7 +41,7 @@ const NotesSettingsWidget = new GObject.Class({
 		let radioBtn1 = builder.get_object('radio1');
 		let radioBtn2 = builder.get_object('radio2');
 		let radioBtn3 = builder.get_object('radio3');
-		switch (SETTINGS.get_string('layout-position')) {
+		switch (this._settings.get_string('layout-position')) {
 			case 'above-all':
 				radioBtn1.set_active(true);
 			break;
@@ -61,18 +60,18 @@ const NotesSettingsWidget = new GObject.Class({
 		//----------------------------------------------------------------------
 
 		let focus_switch = builder.get_object('focus_switch');
-		focus_switch.set_state(SETTINGS.get_boolean('auto-focus'));
+		focus_switch.set_state(this._settings.get_boolean('auto-focus'));
 		focus_switch.connect('notify::active', (widget) => {
-			SETTINGS.set_boolean('auto-focus', widget.active);
+			this._settings.set_boolean('auto-focus', widget.active);
 		});
 
 		//----------------------------------------------------------------------
 
 		// The "hide icon" switch has to be build before the keybinding switch
 		this._hide_switch = builder.get_object('hide_switch');
-		this._hide_switch.set_state(SETTINGS.get_boolean('hide-icon'));
+		this._hide_switch.set_state(this._settings.get_boolean('hide-icon'));
 		this._hide_switch.connect('notify::active', (widget) => {
-			SETTINGS.set_boolean('hide-icon', widget.active);
+			this._settings.set_boolean('hide-icon', widget.active);
 		});
 
 		//----------------------------------------------------------------------
@@ -83,30 +82,30 @@ const NotesSettingsWidget = new GObject.Class({
 
 		// Text entry
 		let keybinding_entry = builder.get_object('keybinding_entry');
-		keybinding_entry.set_sensitive(SETTINGS.get_boolean('use-shortcut'));
+		keybinding_entry.set_sensitive(this._settings.get_boolean('use-shortcut'));
 		keybinding_entry.set_tooltip_text(default_kbs_label);
 
 		builder.get_object('default-kbs-help-1').set_label(default_kbs_label);
 		builder.get_object('default-kbs-help-2').set_label(RELOAD_TEXT);
 
-		if (SETTINGS.get_strv('notes-kb-shortcut') != '') {
-			keybinding_entry.text = SETTINGS.get_strv('notes-kb-shortcut')[0];
+		if (this._settings.get_strv('notes-kb-shortcut') != '') {
+			keybinding_entry.text = this._settings.get_strv('notes-kb-shortcut')[0];
 		}
 
 		// "Apply" button
 		let keybinding_button = builder.get_object('keybinding_button');
-		keybinding_button.set_sensitive(SETTINGS.get_boolean('use-shortcut'));
+		keybinding_button.set_sensitive(this._settings.get_boolean('use-shortcut'));
 
 		keybinding_button.connect('clicked', (widget) => {
-			SETTINGS.set_strv('notes-kb-shortcut', [keybinding_entry.text]);
+			this._settings.set_strv('notes-kb-shortcut', [keybinding_entry.text]);
 		});
 
 		// "Enable shortcut" switch
 		let keybinding_switch = builder.get_object('keybinding_switch');
-		keybinding_switch.set_state(SETTINGS.get_boolean('use-shortcut'));
+		keybinding_switch.set_state(this._settings.get_boolean('use-shortcut'));
 
 		keybinding_switch.connect('notify::active', (widget) => {
-			SETTINGS.set_boolean('use-shortcut', widget.active);
+			this._settings.set_boolean('use-shortcut', widget.active);
 			keybinding_entry.sensitive = widget.active;
 			keybinding_button.sensitive = widget.active;
 			this._hide_switch.sensitive = widget.active;
@@ -116,7 +115,7 @@ const NotesSettingsWidget = new GObject.Class({
 
 		// The default color of the very first note
 		let color_btn = builder.get_object('default_rgb_btn');
-		let colorArray = SETTINGS.get_strv('first-note-rgb');
+		let colorArray = this._settings.get_strv('first-note-rgb');
 		let rgba = new Gdk.RGBA();
 		rgba.red = parseFloat(colorArray[0]);
 		rgba.green = parseFloat(colorArray[1]);
@@ -126,7 +125,7 @@ const NotesSettingsWidget = new GObject.Class({
 
 		color_btn.connect('color-set', (widget) => {
 			rgba = widget.get_rgba();
-			SETTINGS.set_strv('first-note-rgb', [
+			this._settings.set_strv('first-note-rgb', [
 				rgba.red.toString(),
 				rgba.green.toString(),
 				rgba.blue.toString()
@@ -136,14 +135,14 @@ const NotesSettingsWidget = new GObject.Class({
 
 	_connectRadioBtn(strId, widget) {
 		widget.connect('toggled', (widget) => {
-			SETTINGS.set_string('layout-position', strId);
+			this._settings.set_string('layout-position', strId);
 		});
 	},
 
 	//--------------------------------------------------------------------------
 
 	_buildHelpPage(builder) {
-		let help_url = Me.metadata.url.toString();
+		let help_url = this._extension.metadata.url.toString();
 		help_url += "/blob/master/user-help/README.md";
 		builder.get_object('help_btn').set_uri(help_url);
 
@@ -155,7 +154,7 @@ const NotesSettingsWidget = new GObject.Class({
 
 		let reset_button = builder.get_object('reset_btn');
 		reset_button.connect('clicked', (widget) => {
-			SETTINGS.set_boolean('ugly-hack', !SETTINGS.get_boolean('ugly-hack'));
+			this._settings.set_boolean('ugly-hack', !this._settings.get_boolean('ugly-hack'));
 		});
 	},
 
@@ -163,11 +162,11 @@ const NotesSettingsWidget = new GObject.Class({
 
 	_buildAboutPage(builder) {
 		builder.get_object('about_icon').set_from_pixbuf(
-			GdkPixbuf.Pixbuf.new_from_file_at_size(Me.path +
+			GdkPixbuf.Pixbuf.new_from_file_at_size(this._path +
 		                             '/screenshots/about_picture.png', 163, 114)
 		);
 
-		let ext_version = _("Version %s").replace('%s', Me.metadata.version.toString());
+		let ext_version = _("Version %s").replace('%s', this._extension.metadata.version.toString());
 		builder.get_object('label_version').set_label(ext_version)
 
 		let translation_credits = builder.get_object('translation_credits').get_label();
@@ -176,7 +175,7 @@ const NotesSettingsWidget = new GObject.Class({
 			builder.get_object('translation_credits').set_label('');
 		}
 
-		let ext_report_url = Me.metadata.url.toString();
+		let ext_report_url = this._extension.metadata.url.toString();
 		builder.get_object('report_link_button').set_uri(ext_report_url);
 	}
 
@@ -184,12 +183,25 @@ const NotesSettingsWidget = new GObject.Class({
 
 //------------------------------------------------------------------------------
 
-// I guess this is like the "enable" in extension.js : something called each
-// time he user try to access the settings' window
-function buildPrefsWidget() {
-	let widget = new NotesSettingsWidget();
-	return widget.prefs_stack;
+export default class NotesPreferences extends ExtensionPreferences {
+	fillPreferencesWindow(window) {
+		const prefsPage = new Adw.PreferencesPage({
+				title: _("Notes Preferences"),
+				icon_name: "dialog-information-symbolic",
+		});
+
+		const prefsGroup = new Adw.PreferencesGroup({
+			title: "",
+			description: _("Configure the appearance of the extension"),
+		});
+
+		let widget = new NotesSettingsWidget(this);
+
+		prefsGroup.add(widget.prefs_stack);
+		prefsPage.add(prefsGroup);
+		window.add(prefsPage);
+
+		window.set_default_size(625, 650);
+	}
 }
-
 //------------------------------------------------------------------------------
-
